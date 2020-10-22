ecbbbd7440a05aeee605ba87b843db48a4f56a4a
HervalMata <hervalmataamparo@gmail.com>
2020-10-21 22:20:40 -0300

    Multi Auth | Guards for Admin | Auth for Users

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('/admin')->namespace('Admin')->group(function () {
-    Route::get('dashboard', 'AdminController@dashboard');
     Route::match(['get', 'post'], '/', 'AdminController@login');
+    Route::group(['middleware' => ['admin']], function () {
+        Route::get('dashboard', 'AdminController@dashboard');
+    });
 });
 
